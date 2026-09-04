eab41d4c2885bdd6ed0ca6a314762f325ca3599e
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-09-04 12:59:44 +0200

    09.04.

---
+++ Backend/elso.php
@@ -0,0 +1,79 @@
+<?php
+    echo "<h1>Hello world!</h1>";
+    echo "valami \n"; echo "valami
+    " . " más" . "<br>" . "
+    ";
+
+
+    echo 'Kukk';
+
+    $alma = "Jonatán";
+    $Alma = "Idared";
+    $korte = "Vilmos";
+
+
+    echo $korte . " " . $alma . "<br>
+    ";
+    
+    $korte = 3;
+    $alma = false;
+
+    echo $korte . " " . $alma . "<br>";
+
+    var_dump($korte);
+    var_dump($Alma);
+
+    $tomb = [];
+
+    $tomb[] = 12;
+    $tomb[] = 12;
+
+
+    $tomb[1] = 120;
+    $tomb[100] = 100;
+    $tomb[] = "12";
+
+
+    //asszociatív tömb
+
+    $tomb["Géza"] = "Kresz";
+    
+    $tomb["Géza"] = ["Kresz","Mézga","BK" => "Kertész"];
+
+    echo "<pre>";
+    var_dump($tomb);
+    echo "</pre>";
+   
+    echo $tomb[0] . "<br>";
+    echo $tomb["Géza"][0] . "<br>";
+    echo $tomb["Géza"]["BK"] . "<br>";
+
+    echo "Kedvenc Gézánk: " . $tomb["Géza"][1] . "<br>";
+
+    //Formázott kiiratás
+    echo "Kedvenc gyümölcs: $Alma" . "<br>";
+    
+    echo 'Kedvenc gyümölcs: $Alma';
+
+    for($i = 0;$i < 10;$i++)
+    {
+        echo "$i <br>";
+    };
+
+    $i = 0;
+    while($i < 5)
+    {
+        $i++;
+        echo "$i <br>";
+    };
+
+
+    /*
+    do
+    {
+        echo "$i <br>";
+    }
+    while($i > 0);*/
+    
+    
+?>
