0185815b6477155c72b9f03711a6cdf2cc515bee
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-09-14 13:15:43 +0200

    09.14

---
+++ Backend/Órai/09.14/Feladat09.14.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']);?>" method="post" enctype="multipart/form-data">
+        <input type="text" name="szoveg">
+        <input type="submit">
+        <div>
+
+        </div>
+    </form>
+    
+    <?php
+
+        
+
+
+    ?>
+</body>
+</html>
